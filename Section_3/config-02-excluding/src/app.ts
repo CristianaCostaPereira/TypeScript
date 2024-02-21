53662aa8ed3ed67676b73288e964e6a5acaac4e8
Cristiana Pereira <cristiana.vc.pereira@gmail.com>
2024-02-21 11:35:04 +0000

    Strict compilation
    Lesson 43

--- Section_3/config-02-excluding/src/app.ts
+++ Section_3/config-02-excluding/src/app.ts
@@ -1,10 +1,11 @@
-// const userName = 'Maximilian';
-
-// console.log(userName);
-
 const button = document.querySelector('button') !;
 
+function clickHandler(message: string) {
+  console.log('Clicked! ' + message);
+}
+
+
 // comment
-button.addEventListener('click', () => {
-  console.log('Clicked!')
-})
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, 'You are welcome'))
+}
