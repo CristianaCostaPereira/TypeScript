0944f5b5ccdbeb49746ea88576dc63c1036b7b05
Cristiana Pereira <cristiana.vc.pereira@gmail.com>
2024-02-21 17:35:57 +0000

    Creating a first class
    Lesson 59

--- Section_5/cl-interfaces-01-starting-setup/src/app.ts
+++ Section_5/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+// Starts with an uppercase character to make it clear that is a class
+class Department {
+  name: string;
+
+  constructor (n: string) {
+    this.name = n;
+  }
+}
+
+// create department object
+const accounting = new Department('Accounting');
+
+console.log(accounting);
